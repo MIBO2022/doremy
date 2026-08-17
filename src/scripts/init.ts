839d1d7774db12292b312cd3caf6de738865094c
JudyLiao <113348816+MIBO2022@users.noreply.github.com>
2026-08-18 01:29:25 +0800

    Update init.ts

--- src/scripts/init.ts
+++ src/scripts/init.ts
@@ -2,6 +2,7 @@ import { enableCopyrightCopy } from "./copyright-copy";
 import { enableImageProtection } from "./image-protection";
 import { enableImageGalleryLightbox } from "./image-gallery-lightbox";
 
+
 document.addEventListener("astro:page-load", () => {
 
     enableImageProtection();
