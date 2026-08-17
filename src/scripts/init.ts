f45ce695203087d7732878f5c817556caf56dab3
JudyLiao <113348816+MIBO2022@users.noreply.github.com>
2026-08-18 01:44:54 +0800

    Update init.ts

--- src/scripts/init.ts
+++ src/scripts/init.ts
@@ -1,14 +1,12 @@
 import { enableCopyrightCopy } from "./copyright-copy";
 import { enableImageProtection } from "./image-protection";
-import "./image-gallery-lightbox";
-
+import { enableImageGalleryLightbox } from "./image-gallery-lightbox";
 
 document.addEventListener("astro:page-load", () => {
-
     enableImageProtection();
+    enableImageGalleryLightbox();
 
     if (window.location.pathname.startsWith("/blog/")) {
         enableCopyrightCopy();
     }
-
 });
