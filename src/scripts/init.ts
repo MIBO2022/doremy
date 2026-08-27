f52cde42e9411e477b65ddde8634b4957621658d
JudyLiao <113348816+MIBO2022@users.noreply.github.com>
2026-08-28 01:35:46 +0800

    Update init.ts
    import './platform-comparison-filter';

--- src/scripts/init.ts
+++ src/scripts/init.ts
@@ -1,6 +1,7 @@
 import { enableCopyrightCopy } from "./copyright-copy";
 import { enableImageProtection } from "./image-protection";
 import { enableImageGalleryLightbox } from "./image-gallery-lightbox";
+import './platform-comparison-filter';
 
 document.addEventListener("astro:page-load", () => {
     enableImageProtection();
